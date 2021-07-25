21b86f0bca09203346cb70edd1001f66d83227f3
Armen <iamemerald04@mail.ru>
2021-07-25 11:55:11 +0400

    Middle numbers

---
+++ Middle-number.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[12]:
+
+
+list = [1, 2, 3, 4, 5, 6,7]
+
+first_middle = int(len(list) / 2) - 1 # = 3 - 1 = 2
+second_middle = int(len(list) / 2)    # = 3
+last_middle = int(len(list) / 2) + 1 # = 3 + 1 = 4
+print(list[first_middle])  # 3
+print(list[second_middle]) # 4
+print(list[last_middle]) # 5
+
+
+# In[ ]:
+
+
+
+
